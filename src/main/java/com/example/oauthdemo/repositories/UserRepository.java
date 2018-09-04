a9f2b103f815b82d360ea233aaef12b76d04c0e6
hilmiat <hilmi.tawakal@gmail.com>
2018-09-04 20:45:10 +0700

    source saya

--- src/main/java/com/example/oauthdemo/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.oauthdemo.repositories;
-
-import com.example.oauthdemo.entities.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User,Long> {
-    public User findUserByUsername(String username);
-}
